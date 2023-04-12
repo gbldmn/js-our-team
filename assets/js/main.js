812c3a7d3a3110b8e6f7a933b472653144f9aa25
glbdmn <gabrieledamiani@hotmail.it>
2023-04-12 16:10:57 +0200

    fine esercizio con bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,21 +30,51 @@ const team = [
       image: 'barbara-ramos-graphic-designer.jpg',
     },
   ];
+
+// let arrayMembri = ['6','6','6','6','6','6'];
+// for(let k=0; k < arrayMembri.length; k++) {
+//     console.log(arrayMembri.length)
+//     let pic = document.createElement('img'); 
+//     pic.src = team.image;
+//     console.log(pic.src)
+// }
+
+// let photo = newArray[''];
+// let str;
+
+// let photo[0]="img/angoli.jpg";
+// let photo[1]="img/equazioni.jpg";
+// let photo[2]="img/frazioni.jpg";
+// let photo[3]="img/calcolatrice.jpg";
+// let photo[4]="img/annobisestile.jpg";
+// let photo[5]="img/palindroma.jpg";
+
+// let arrayImmagini = ['img/angela-caroll-chief-editor.jpg','img/angela-lopez-social-media-manager.jpg','img/barbara-ramos-graphic-designer.jpg','img/scott-estrada-developer.jpg','img/walter-gordon-office-manager.jpg','img/wayne-barnett-founder-ceo.jpg']
+// function foto() {    
+//  for(let k=0; k < arrayImmagini.length; k++)
+//     console.log(arrayImmagini[k])
+//     var img = document.createElement("img");
+    
+    
+//     document.body.appendChild(img);
+// }
+// foto()
+
   
-for(let i=0; i < team.length; i++) {
-    console.log(team)
-    let elementiSingoli = team[i]
-    console.log(elementiSingoli)
-    document.querySelector('.row').innerHTML += `
-    <div class="card" style="width: 18rem;">
-       <img src="..." class="card-img-top" alt="...">
-       <div class="card-body">
-       <h5 class="card-title">Card title</h5>
-       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-       <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>`
-}
+ for(let i=0; i < team.length; i++) {
+     console.log(team)
+     let elementiSingoli = team[i]
+     console.log(elementiSingoli)
+     document.querySelector('.row').innerHTML += `
+     <div class="card border-black" style="width: ;">
+     <img src="img/${elementiSingoli.image}"  alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${elementiSingoli.name}</h5>
+        <p class="card-text">${elementiSingoli.role}</p>
+        <a href="#" class="btn btn-primary">clicca</a>
+       </div>
+     </div>`
+ }
   
   
   
