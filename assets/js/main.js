ed4320723311c6428b15d1ffb99e3b346ceb89f6
glbdmn <gabrieledamiani@hotmail.it>
2023-04-12 13:01:34 +0200

    creato array oggetti e ciclato ogni elemeno all'in

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+  
+for(let i=0; i < team.length; i++) {
+    console.log(team)
+    let elementiSingoli = team[i]
+    console.log(elementiSingoli)
+}
+  
+  
+  
+  
+  
+  
