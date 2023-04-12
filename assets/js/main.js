3d4a27ebb7a84086531271765d6d6ea4470005fb
glbdmn <gabrieledamiani@hotmail.it>
2023-04-12 13:07:47 +0200

    inserisco all'interno del ciclo for la card

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,15 @@ for(let i=0; i < team.length; i++) {
     console.log(team)
     let elementiSingoli = team[i]
     console.log(elementiSingoli)
+    document.querySelector('.row').innerHTML += `
+    <div class="card" style="width: 18rem;">
+       <img src="..." class="card-img-top" alt="...">
+       <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+       <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>`
 }
   
   
